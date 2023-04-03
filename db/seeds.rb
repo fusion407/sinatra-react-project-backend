bac3b933a991b6e6919fc20267d2ca43b7f88bac
fusion407 <fusion407@gmail.com>
2023-04-03 18:21:59 -0500

    minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Fullset.create([
     {
         title: "Excision Lost Lands 2022 Detox Set",
         rating: 5,
-        video_link: "https://youtu.be/_XsLl8yLpq4",
+        video_link: "https://www.youtube.com/embed/watch?v=_XsLl8yLpq4",
         artist_id: 1,
         event_id: 1,
         genre_id: 1,
@@ -13,7 +13,7 @@ Fullset.create([
     {
         title: "Deadmau5 EDC Las Vegas 2021",
         rating: 4,
-        video_link: "https://youtu.be/AjiSzMFEDgo",
+        video_link: "https://www.youtube.com/embed/watch?v=AjiSzMFEDgo",
         artist_id: 2,
         event_id: 2,
         genre_id: 2,
